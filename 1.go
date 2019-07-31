de46e0f36d0e2dac347d7080e6874f7401ca1490
Dimas Febri <dfebri446@gmail.com>
2019-08-01 03:25:37 +0700

    update 1, use sort

--- 1.go
+++ 1.go
@@ -3,44 +3,51 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sort"
 )
 
 func main() {
-	sortWord("omama")
+	sortWord("osama")
 }
 
 func sortWord(w string) {
 	arrWord := strings.Split(strings.Replace(w, " ", "", -1), "")
 	vocal := []string{"a", "i", "u", "e", "o"}
-	var life, dead string
+	var life, dead []string
+	var hasil string
 
 	for _, value := range arrWord {
 		for i, v := range vocal {
 			if value == v {
-				life += value
+				life = append(life, value)
 				break
 			} else if i == len(vocal) - 1 {
-				dead += value
+				dead = append(dead, value)
 			}
 		}
 	}
 
-	alphabet := []string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
-	var hasil string
-	for _, y := range alphabet {
-		for _, x := range strings.Split(life, "") {
-			if y == x {
-				hasil += x
-			}
-		}
-	}
-	for _, y := range alphabet {
-		for _, x := range strings.Split(dead, "") {
-			if y == x {
-				hasil += x
-			}
-		}
-	}
+	// ########### SORT MANUAL ###########
+	// alphabet := []string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+	// for _, y := range alphabet {
+	// 	for _, x := range life {
+	// 		if y == x {
+	// 			hasil += x
+	// 		}
+	// 	}
+	// }
+	// for _, y := range alphabet {
+	// 	for _, x := range dead {
+	// 		if y == x {
+	// 			hasil += x
+	// 		}
+	// 	}
+	// }
+
+	// ########## USE sort #############
+	sort.Strings(life)
+	sort.Strings(dead)
+	hasil = strings.Join(life, "") + strings.Join(dead, "")
 
 	fmt.Println(hasil)
 }
