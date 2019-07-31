006d36568a21c1dd7fb682b731630ef467889f32
Dimas Febri <dfebri446@gmail.com>
2019-08-01 02:43:25 +0700

    no 0

--- 0.go
+++ 0.go
@@ -5,16 +5,41 @@ import (
 	"strings"
 )
 
+func main() {
+	alphabet("oma ma")
+}
+
 func alphabet(w string) {
 	arrWord := strings.Split(w, "")
+	vocal := []string{"a", "i", "u", "e", "o"}
+	var arrVocal []string
+	var arrConst []string
 
-	fmt.Println(count(arrWord))
+	for _, value := range arrWord {
+		for i, v := range vocal {
+			if value == v {
+				if len(arrVocal) != 0 {
+					for u, y := range arrVocal {
+						if value == y {
+							break
+						} else if u == len(arrVocal) - 1 {
+							arrVocal = append(arrVocal, value)
+						} 
+					}
+				} else {
+					arrVocal = append(arrVocal, value)
+				}
+				break
+			} else if i == len(vocal) - 1 {
+				arrConst = append(arrConst, value)
+			}
+		}
+	}
+
+	fmt.Println("huruf mati :", count(arrConst))
+	fmt.Println("huruf hidup :", count(arrVocal))
 }
 
 func count(s []string) int {
 	return len(s)
-}
-
-func main() {
-	alphabet("omama")
 }
