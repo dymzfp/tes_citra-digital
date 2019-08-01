136e8b93d42c124f6bb5d3d3d7915b26b070b565
Dimas Febri <dfebri446@gmail.com>
2019-08-01 11:30:32 +0700

    update 2

--- 2.go
+++ 2.go
@@ -3,12 +3,44 @@ package main
 import (
 	"fmt"
 	"strings"
+	"encoding/json"
+	"net/http"
 )
 
-func main() {
-	hidup, mati := countAlpha("omama") 
+type Hasil struct {
+	Hidup 	int 	`json:"Hidup"`
+	Mati 	int 	`josn:"Mati"`
+}
 
-	fmt.Println(hidup, mati)
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome"))
+	})
+	http.HandleFunc("/count", HandleCount)
+
+	fmt.Println("Server running")
+	http.ListenAndServe(":1234", nil)
+}
+
+func HandleCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		wordInput := r.FormValue("input")
+		if wordInput == "" {
+			http.Error(w, "Input not found", http.StatusBadRequest)
+			return
+		}
+
+		h, m := countAlpha(wordInput)
+		data  := Hasil{h, m}
+
+		json.NewEncoder(w).Encode(data)
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+	return
 }
 
 func countAlpha(w string) (x, y int) {
